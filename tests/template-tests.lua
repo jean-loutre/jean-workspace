df17baec7a320fc9abebc9573c2dee7fa4b30cf5
Jean-Loutre <jean-loutre@gmx.com>
2022-12-07 11:35:34 +0100

    test: test loading a table of templates

--- tests/template-tests.lua
+++ tests/template-tests.lua
@@ -8,4 +8,11 @@ function Suite.load_function()
 	assert_equals(load_templates(caiman_shredder):to_list(), { caiman_shredder })
 end
 
+function Suite.load_table()
+	local function caiman_shredder() end
+	local function caiman_electrifier() end
+	local templates = { caiman_shredder, caiman_electrifier }
+
+	assert_equals(load_templates(templates):to_list(), templates)
+end
 return Suite
