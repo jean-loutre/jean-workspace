6781176edeac1d2d0ec02230a743f8dc3c6f332e
Jean-Loutre <jean-loutre@gmx.com>
2022-12-07 11:33:20 +0100

    test: test loading a single function template

--- tests/template-tests.lua
+++ tests/template-tests.lua
@@ -1,5 +1,11 @@
 local TestSuite = require("jnvim.test-suite")
+local load_templates = require("jworkspace.template").load_templates
 
 local Suite = TestSuite()
+
+function Suite.load_function()
+	local function caiman_shredder() end
+	assert_equals(load_templates(caiman_shredder):to_list(), { caiman_shredder })
+end
 
 return Suite
