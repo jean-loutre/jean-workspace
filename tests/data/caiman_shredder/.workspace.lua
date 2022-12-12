818a58e2aac2d5c347d6cf5c1fc900e4c3f4bed7
Jean-Loutre <jean-loutre@gmx.com>
2022-12-12 09:26:36 +0100

    refactor: clean template tests

--- tests/data/caiman_shredder/.workspace.lua
+++ tests/data/caiman_shredder/.workspace.lua
@@ -1,3 +1,3 @@
 return {
-	power = "12kw",
+	rpm = 15264,
 }
--- tests/template-tests.lua
+++ tests/template-tests.lua
@@ -18,49 +18,56 @@ function Suite.load_function()
 end
 
 function Suite.load_table_import()
-	local function caiman_shredder(root, name)
+	local function caiman_shredder(root, name, config)
 		assert_equals(root, "/caiman_shredder")
 		assert_equals(name, "caiman_shredder")
+		assert_equals(config, {})
 		return { rpm = 12564 }
 	end
 
-	local function caiman_electrifier(root, name)
+	local function caiman_electrifier(root, name, config)
 		assert_equals(root, "/caiman_shredder")
 		assert_equals(name, "caiman_shredder")
+		assert_equals(config, { rpm = 12564 })
 		return { power = "12kw" }
 	end
 
-	local templates = { caiman_shredder, caiman_electrifier, blade_count = 10 }
-	local config = load_config("/caiman_shredder", "caiman_shredder", {}, templates)
+	local template = { caiman_shredder, caiman_electrifier, blade_count = 10 }
+	local config = load_config("/caiman_shredder", "caiman_shredder", {}, template)
 	assert_equals(config, { rpm = 12564, power = "12kw", blade_count = 10 })
 end
 
 function Suite.load_lua_module()
-	local function caiman_shredder()
+	local function caiman_shredder(root, name, config)
+		assert_equals(root, "/caiman_shredder")
+		assert_equals(name, "caiman_shredder")
+		assert_equals(config, {})
 		return { power = "12kw" }
 	end
+
 	package.loaded["caiman_shredder"] = caiman_shredder
 
-	-- Default loader is lua module
-	assert_equals(load_config("", "", {}, "caiman_shredder"), { power = "12kw" })
+	local config = load_config("/caiman_shredder", "caiman_shredder", {}, "caiman_shredder")
+	assert_equals(config, { power = "12kw" })
+
+	package.loaded["caiman_shredder"] = nil
 end
 
 function Suite.load_lua_file()
-	local template = load_config("", "", {}, "tests/data/templates/caiman_shredder.lua")
-	assert_equals(template, { power = "12kw" })
+	local config = load_config("", "", {}, "tests/data/templates/caiman_shredder.lua")
+	assert_equals(config, { power = "12kw" })
 end
 
 function Suite.load_yaml_file()
 	for extension in iter({ "yml", "yaml", "json" }) do
-		local template = load_config("", "", {}, "tests/data/templates/caiman_shredder." .. extension)
-
-		assert_equals(template, { rpm = 15264 })
+		local config = load_config("", "", {}, "tests/data/templates/caiman_shredder." .. extension)
+		assert_equals(config, { rpm = 15264 })
 	end
 end
 
 function Suite.load_glob()
-	local result = load_config("", "", {}, "tests/data/templates/caiman_shredder.*")
-	assert_equals(result, {
+	local config = load_config("", "", {}, "tests/data/templates/caiman_shredder.*")
+	assert_equals(config, {
 		power = "12kw",
 		rpm = 15264,
 	})
