1197581de310938e9015883ee5f95fc58a2873f0
Jean-Loutre <jean-loutre@gmx.com>
2022-12-13 10:51:56 +0100

    doc: document pylsp builtin

--- lua/jworkspace/builtin/lsp/pylsp.lua
+++ lua/jworkspace/builtin/lsp/pylsp.lua
@@ -1,9 +1,11 @@
---- Default configuration for pylsp language server
---- @module 'jworkspace.builtin.lsp.pylsp'
-
---- Add default configuration for pylsp language server if it's found on the system.
---- @param config jlua.Map
---- @return nil
+--- Default configuration for pylsp language server.
+--
+-- If pylsp is found on the system, will add a default configuration to attach
+-- a pylsp server on python files in the workspace. If the config already have
+-- a ["lsp"]["servers"]["pylsp"] configuration key, it will do nothing.
+--
+-- You can force pylsp off by setting lsp.servers.pylsp key to false in your
+-- workspace configuration.
 return function(_, _, config)
 	if not vim.fn.executable("pylsp") then
 		return nil
